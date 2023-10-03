20b401b6914b53f60384699cf0857d7c8c8035a7
surister <surister98@gmail.com>
2023-10-03 12:12:27 +0200

    Improve storedPreferences, it now automatically detects changes and stores them.

--- src/store/storedPreferences.js
+++ src/store/storedPreferences.js
@@ -5,35 +5,41 @@ import {useTheme} from "vuetify";
 
 const localStorageKey = 'crateDB@storedPreferences'
 export const useStoredPreferencesStore = defineStore('storedPreferences', () => {
-    const state = reactive({
-      theme: 'light'
-    })
+  const state = reactive({
+    theme: 'light'
+  })
+  const theme = useTheme()
 
-    function load() {
-      const stored = localStorage.getItem(localStorageKey)
-      const data = stored ? JSON.parse(stored) : {}
-      const theme = useTheme()
+  function load() {
+    const stored = localStorage.getItem(localStorageKey)
+    const data = stored ? JSON.parse(stored) : {}
 
-      Object.assign(state, data)
 
-      // State initialized from stored Preferences at this point, do stuff below.
+    Object.assign(state, data)
 
-      theme.global.name.value = state.theme;
+    // State initialized from stored Preferences at this point, do stuff below.
+    theme.global.name.value = state.theme
+  }
+
+  function save() {
+    if (state.theme != theme.global.name.value){
+      theme.global.name.value = state.theme
     }
-    function save(){
-      localStorage.setItem(localStorageKey, JSON.stringify(state, null, 2))
-    }
-    load()
 
-    // Whenever the state of any stored preferences changes, we store it.
-    watch(
-      state, () => save()
-    )
+    localStorage.setItem(localStorageKey, JSON.stringify(state, null, 2))
+  }
 
-    return {
-      ...toRefs(state),
-      load,
-      save
-    }
+  load()
+
+  // Whenever the state of any stored preferences changes, we store it.
+  watch(
+    state, () => save()
+  )
+
+  return {
+    ...toRefs(state),
+    load,
+    save
+  }
   }
 )
