65833294c16110f12ca3128007eccee9a52ba2e8
surister <surister98@gmail.com>
2023-10-03 12:12:47 +0200

    Crate list item link component

---
+++ src/components/settings/ListItemLink.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  preprendIcon: String,
+  appendIcon: String,
+  title: String,
+  linkTitle: String,
+  linkTo: String,
+})
+</script>
+
+<template>
+  <v-list-item>
+    <template v-slot:prepend>
+      <v-icon :icon="preprendIcon"></v-icon>
+      <v-label class="ml-4">{{ title }}</v-label>
+    </template>
+
+    <template v-slot:append>
+      <v-btn
+        :href="linkTo"
+        color="grey-darken-1"
+        :append-icon="appendIcon"
+        variant="text">
+        {{ linkTitle }}
+      </v-btn>
+    </template>
+  </v-list-item>
+</template>
+
+<style scoped>
+
+</style>
