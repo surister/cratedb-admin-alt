48e3fffaf56ab9009c66ae06f33924f981b08f6b
surister <surister98@gmail.com>
2023-10-03 12:11:48 +0200

    Add settings drawer enabler to bar

--- src/components/bar/Bar.vue
+++ src/components/bar/Bar.vue
@@ -1,15 +1,20 @@
 <script setup>
-
 import ThemeToggle from "@/components/bar/ThemeToggle.vue";
+import VerticalDivider from "@/components/VerticalDivider.vue";
+import {useSettingsStore} from "@/store/settingsStore";
+
+const settingsStore = useSettingsStore()
 </script>
 
 <template>
 <v-app-bar :elevation="2">
   <template v-slot:append>
     <v-btn icon="mdi-heart"></v-btn>
-    <v-btn icon="mdi-magnify"></v-btn>
-    <v-btn icon="mdi-dots-vertical"></v-btn>
-    <theme-toggle></theme-toggle>
+    <vertical-divider></vertical-divider>
+    <v-btn variant="flat" color="primary">Crate</v-btn>
+    <vertical-divider></vertical-divider>
+    <v-btn icon="mdi-cog" @click="settingsStore.settingsDrawerToggle = !settingsStore.settingsDrawerToggle"></v-btn>
+    <!--    <theme-toggle></theme-toggle>-->
   </template>
 </v-app-bar>
 </template>
