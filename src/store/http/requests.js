4c8088ca4a216062aa489979ebf34d42d03c4895
surister <surister98@gmail.com>
2023-10-03 20:34:40 +0200

    Add options to requestCrate function

--- src/store/http/requests.js
+++ src/store/http/requests.js
@@ -1,15 +1,13 @@
-export async function requestCrate(stmt) {
-  const response = await fetch(
-    'http://localhost:4201/_sql',
-    {
-      method: 'POST',
-      cache: 'no-cache',
-      body: JSON.stringify({'stmt': stmt}),
-      headers: {
-        "Content-Type": "application/json",
-
-      },
-    }
-  )
-  return await response.json();
+export async function requestCrate(stmt, options) {
+  return await fetch(
+      'http://localhost:4201/_sql' + '?' + options,
+      {
+        method: 'POST',
+        cache: 'no-cache',
+        body: JSON.stringify({'stmt': stmt}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 }
