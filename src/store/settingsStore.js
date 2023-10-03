689a59db98015ee3add758371dbd829addc78506
surister <surister98@gmail.com>
2023-10-03 12:13:08 +0200

    Crate settings bar store

---
+++ src/store/settingsStore.js
@@ -0,0 +1,14 @@
+// Utilities
+import {defineStore} from 'pinia'
+import {reactive, toRefs} from 'vue';
+
+export const useSettingsStore = defineStore('settings', () => {
+    const state = reactive({
+      settingsDrawerToggle: false
+    })
+
+    return {
+      ...toRefs(state),
+    }
+  }
+)
