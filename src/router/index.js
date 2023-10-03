2485e4f40bd55be29d490f6a7f539ab86ab31c43
surister <surister98@gmail.com>
2023-10-03 14:51:31 +0200

    Add console view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 
+
 const routes = [
   {
     path: '/',
@@ -8,11 +9,16 @@ const routes = [
     children: [
       {
         path: '',
-        name: 'Home',
+        name: 'home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+      {
+        path: '/console',
+        name: 'console',
+        component: () => import('@/views/Console.vue')
       },
     ],
   },
