0655815e8c2b4a0558d6dfda3e5527b0d1ac0c66
surister <surister98@gmail.com>
2023-10-04 20:17:36 +0200

    Add Health Info pop over when click the health

--- src/components/bar/info/Health.vue
+++ src/components/bar/info/Health.vue
@@ -1,14 +1,27 @@
 <script setup>
+import HealthInfoPopOver from "@/components/bar/info/HealthInfoPopOver.vue";
 
+import {ref} from "vue";
+
+let menu = ref(false)
 </script>
 
 <template>
+  <v-row>
+      <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-label :clickable="true" v-bind="props">Health</v-label>
+        </template>
+        <health-info-pop-over></health-info-pop-over>
+      </v-menu>
 
-  <v-label>Health</v-label>
-  <v-radio value="1" :readonly="false" class="mr-1"></v-radio>
-  <v-label>Data</v-label>
-  <v-radio value="1" :readonly="false" class="mr-4"></v-radio>
-
+    <v-radio value="1" :readonly="false" class="mr-1"></v-radio>
+    <v-label>Data</v-label>
+    <v-radio value="1" :readonly="false" class="mr-4"></v-radio>
+  </v-row>
 </template>
 
 <style scoped>
---
+++ src/components/bar/info/HealthInfoPopOver.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <v-card min-width="300" class="pa-5">
+
+        Add relevant data here
+
+      </v-card>
+</template>
+
+<style scoped>
+
+</style>
