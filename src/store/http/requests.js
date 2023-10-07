923e4f436379650138e78d1ced366cef3aee85de
surister <surister98@gmail.com>
2023-10-07 20:33:13 +0200

    Make requests from the general setting's url instead of hardcoded one

--- src/store/http/requests.js
+++ src/store/http/requests.js
@@ -1,13 +1,27 @@
-export async function requestCrate(stmt, options) {
+import {useStoredPreferencesStore} from "@/store/storedPreferences";
+
+export async function requestCrate(stmt, queryParams = '', stmtReplacedParams= {}) {
+  const storedPreferences = useStoredPreferencesStore()
+  let url = storedPreferences.general.masterNodeUrl + '/_sql'
+  if (queryParams) {
+    url = url + '?' + queryParams
+  }
+
+  if (stmtReplacedParams) {
+    Object.entries(stmtReplacedParams).map(entry => {
+      stmt = stmt.replace(entry[0], entry[1])
+    });
+  }
+
   return await fetch(
-      'http://localhost:4201/_sql' + '?' + options,
-      {
-        method: 'POST',
-        cache: 'no-cache',
-        body: JSON.stringify({'stmt': stmt}),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    url,
+    {
+      method: 'POST',
+      cache: 'no-cache',
+      body: JSON.stringify({'stmt': stmt}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
