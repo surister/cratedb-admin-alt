ac2b57d22e4782176a17c8776bcbfc6fd6a28552
surister <surister98@gmail.com>
2023-10-13 19:52:12 +0200

    Add CollapsibleText and DialogText, two components whose purpose is to show objects and arrays

---
+++ src/components/objectrepresentation/CollapsibleText.vue
@@ -0,0 +1,60 @@
+<script setup>
+import {computed} from "vue";
+import {isArray, isObject} from "@/store/utils";
+
+const props = defineProps({object: Object, type: String})
+
+const is_array = computed(() => props.type === 'array')
+const is_object = computed(() => props.type === 'object')
+</script>
+
+<template>
+<span class="d-inline-block my-3">
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-title expand-icon="">
+          <template v-if="type == 'array'">
+            Array [{{ object.length }}]
+          </template>
+          <template v-else>
+            Object [1]
+          </template>
+          <template v-slot:actions="{ expanded }"></template>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text v-if="is_array" style="max-height: 200px" class="overflow-y-auto">[
+                       <template v-for="k in object" :key="k">
+                          <template v-if="isObject(k)">
+                            <collapsible-text :object="k"></collapsible-text>,
+                          </template>
+                         <template v-else>{{ k }}, </template>
+                       </template>
+          ]
+        </v-expansion-panel-text>
+        <v-expansion-panel-text class="overflow-x-auto" v-else>
+          {
+          <template v-for="entry in Object.entries(object)" :key="entry">
+            <template
+              v-if="isObject(entry[1])">
+              <span><strong>{{ entry[0] }}</strong>: <collapsible-text
+                :object="entry[1]"></collapsible-text></span>
+            </template>
+            <template
+              v-else-if="isArray(entry[1])">
+              <p>array: <collapsible-text :object="entry[1]" type="array"></collapsible-text></p>
+            </template>
+            <template v-else>
+              <p><strong>{{ entry[0] }}</strong>: {{ entry[1] }},</p>
+            </template>
+          </template>
+          }
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+  </v-expansion-panels>
+</span>
+</template>
+
+<style scoped>
+.v-expansion-panel-title, .v-expansion-panel-title--active {
+  padding: 8px 12px;
+}
+</style>
---
+++ src/components/objectrepresentation/DialogText.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  object: Object
+})
+const object_name = computed(()=>`Object[${Object.entries(props.object).length}]`)
+</script>
+
+<template>
+  <v-dialog max-width="600">
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" :text="object_name"></v-btn>
+    </template>
+    <template v-slot:default="{ isActive }">
+      <v-card :title="object_name">
+        <v-card-text>
+          <v-code class="overflow-y-auto" tag="pre">
+            {{ object }}
+          </v-code>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            text="Close"
+            @click="isActive.value = false"
+          ></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<style scoped>
+
+</style>
