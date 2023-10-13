ef2c0522c01ce5361c3cbc1f5895b54b122d59ab
surister <surister98@gmail.com>
2023-10-13 19:51:06 +0200

    Add component to show Bold titled text followed by normal info text

---
+++ src/components/text/TitledText.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  text: String,
+
+})
+</script>
+
+<template>
+<p>{{ title }}: <strong>{{ text }}</strong></p>
+</template>
+
+<style scoped>
+
+</style>
