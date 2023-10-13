524c0c68943ea3743282d4812264f28f789a5291
surister <surister98@gmail.com>
2023-10-13 19:45:59 +0200

    Add headers to Cluster view

--- src/views/Cluster.vue
+++ src/views/Cluster.vue
@@ -1,10 +1,13 @@
 <script setup>
-
-import Nodelist from "@/components/nodelist.vue";
+import Nodelist from "@/components/NodeTable.vue";
 </script>
 
 <template>
-<nodelist></nodelist>
+  <v-row align="center" class="mt-2">
+    <v-col><h1>Cluster</h1></v-col>
+    <nodelist class="mt-2"></nodelist>
+  </v-row>
+
 </template>
 
 <style scoped>
