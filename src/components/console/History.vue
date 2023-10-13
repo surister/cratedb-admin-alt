cad8e396e8fa71f2e2d7ad8c221fa3064fbd4427
surister <surister98@gmail.com>
2023-10-13 19:44:49 +0200

    Add tooltip (hints) to queries on hover

--- src/components/console/History.vue
+++ src/components/console/History.vue
@@ -5,27 +5,34 @@ import {useStoredPreferencesStore} from "@/store/storedPreferences";
 const consoleStore = useConsoleStore()
 const storedPreferences = useStoredPreferencesStore()
 const historyHeader = [
-    {title: 'Query history', align: 'start', key:'query'}, {key:'actions'}
+  {title: 'Query history', align: 'start', key: 'query'}, {key: 'actions'}
 ]
 </script>
 
 <template>
   <v-card>
     <v-data-table
-        items-per-page="5"
-        item-key="query"
-        :headers="historyHeader"
-        :items="storedPreferences.console.queryHistory">
+      items-per-page="5"
+      item-key="query"
+      :headers="historyHeader"
+      :items="storedPreferences.console.queryHistory">
       <template v-slot:item="{ item }">
         <tr>
           <td v-ripple
               @click="consoleStore.content = item.query"
               class="spec text-truncate text-no-wrap"
               style="max-width: 15vw; min-width: 15vw">
-            {{ item.query }}
+            <v-tooltip :text="item.query">
+              <template v-slot:activator="{ props }">
+                <span v-bind="props">
+                  {{ item.query }}
+                </span>
+              </template>
+            </v-tooltip>
           </td>
           <td>
-            <v-btn flat icon="mdi-delete" size="small" @click="storedPreferences.deleteHistory(item)"></v-btn>
+            <v-btn flat icon="mdi-delete" size="small"
+                   @click="storedPreferences.deleteHistory(item)"></v-btn>
           </td>
         </tr>
       </template>
