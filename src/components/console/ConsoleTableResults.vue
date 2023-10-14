4857cc4ed81c6625170ce0485e750722d9d1579a
surister <surister98@gmail.com>
2023-10-14 19:06:00 +0200

    Add title + Fix when there is no data yet.

--- src/components/console/ConsoleTableResults.vue
+++ src/components/console/ConsoleTableResults.vue
@@ -2,9 +2,9 @@
 import {useConsoleStore} from "@/store/consoleStore";
 import {adaptVTableHeader, adaptVTableItems, isArray, isObject} from "@/store/utils";
 import DialogText from "@/components/objectrepresentation/DialogText.vue";
-import CollapsibleText from "@/components/objectrepresentation/CollapsibleText.vue";
 
 const consoleStore = useConsoleStore()
+
 function colorObject(object) {
   switch (typeof object) {
     case "string":
@@ -21,10 +21,14 @@ function colorObject(object) {
 </script>
 
 <template>
-<v-card>
-    <v-data-table
+<v-card v-if="consoleStore.response.data.rows.length !== 0 && consoleStore.response.data.rows[0].length !== 0">
+  <v-data-table
     :items="adaptVTableItems(consoleStore.response.data.rows, consoleStore.response.data.headers)"
-    :headers="adaptVTableHeader(consoleStore.response.data.headers)">
+    :headers="adaptVTableHeader(consoleStore.response.data.headers)"
+  :items-per-page="5">
+    <template v-slot:top>
+      <v-toolbar flat><v-toolbar-title>Query data response.</v-toolbar-title></v-toolbar>
+    </template>
     <template v-slot:item="{ item }">
       <tr>
         <td v-for="it, index in item" :key="index">
