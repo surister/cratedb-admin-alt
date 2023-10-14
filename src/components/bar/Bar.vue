eba34e3a72ed6c97fda7e5a3ea4654a1698f6c6b
surister <surister98@gmail.com>
2023-10-14 20:36:38 +0200

    Adapt bar's title to ribbon

--- src/components/bar/Bar.vue
+++ src/components/bar/Bar.vue
@@ -8,17 +8,20 @@ const global_store = useGlobalStore()
 </script>
 
 <template>
-<v-app-bar :elevation="2" title="CrateDB Alternative panel">
-  <template v-slot:append>
-    <vertical-divider></vertical-divider>
-    <info-group></info-group>
-    <vertical-divider></vertical-divider>
-    <v-btn variant="flat" color="primary">Crate</v-btn>
-    <vertical-divider></vertical-divider>
-    <v-btn icon="mdi-cog"
-           @click="global_store.settingsDrawerToggle = !global_store.settingsDrawerToggle"></v-btn>
-  </template>
-</v-app-bar>
+  <v-app-bar :elevation="2">
+    <template v-slot:title>
+      <span class="ml-10">CrateDB Alternative panel</span>
+    </template>
+    <template v-slot:append>
+      <vertical-divider></vertical-divider>
+      <info-group></info-group>
+      <vertical-divider></vertical-divider>
+      <v-btn variant="flat" color="primary">Crate</v-btn>
+      <vertical-divider></vertical-divider>
+      <v-btn icon="mdi-cog"
+             @click="global_store.settingsDrawerToggle = !global_store.settingsDrawerToggle"></v-btn>
+    </template>
+  </v-app-bar>
 </template>
 
 <style scoped>
