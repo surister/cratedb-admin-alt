581bf1ebabc39733d10f8c652afa1c9615f969a4
surister <surister98@gmail.com>
2023-10-14 20:36:16 +0200

    Add alpha ribbon to bar

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,25 @@
 <script setup>
   //
 </script>
+
+<style>
+body:after{
+  content: "Alpha";
+  position: fixed;
+  width: 80px;
+  height: 25px;
+  background: #EE8E4A;
+  top: 7px;
+  left: -20px;
+  text-align: center;
+  font-size: 13px;
+  font-family: sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #fff;
+  line-height: 27px;
+  transform:rotate(-45deg);
+  z-index: 999999;
+}
+
+</style>
