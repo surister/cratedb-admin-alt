a5a6aa98bdea60e7395d0c1910d99c577e9094fe
surister <surister98@gmail.com>
2023-10-15 12:46:19 +0200

    Style bar title with <h2>

--- src/components/bar/Bar.vue
+++ src/components/bar/Bar.vue
@@ -10,13 +10,13 @@ const global_store = useGlobalStore()
 <template>
   <v-app-bar :elevation="2">
     <template v-slot:title>
-      <span class="ml-10">CrateDB Alternative panel</span>
+      <h2 class="ml-10"> CrateDB Alternative panel</h2>
     </template>
     <template v-slot:append>
       <vertical-divider></vertical-divider>
       <info-group></info-group>
       <vertical-divider></vertical-divider>
-      <v-btn variant="flat" color="primary">Crate</v-btn>
+      <v-btn variant="flat">Crate</v-btn>
       <vertical-divider></vertical-divider>
       <v-btn icon="mdi-cog"
              @click="global_store.settingsDrawerToggle = !global_store.settingsDrawerToggle"></v-btn>
