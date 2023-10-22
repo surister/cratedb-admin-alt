b649fd483343e13ba9f8d0edeae4595fcc4caa74
surister <surister98@gmail.com>
2023-10-22 19:42:28 +0200

    Clean up dead code from query watcher

--- src/components/console/QueryWatcher.vue
+++ src/components/console/QueryWatcher.vue
@@ -1,18 +1,7 @@
 <script setup>
-import {computed, ref} from "vue";
 import {useConsoleStore} from "@/store/consoleStore";
 
-const button = ref()
-
-const selectedItems = ref('')
-
 const consoleStore = useConsoleStore()
-
-const items = [
-  {value: 'Not watch'},
-  {value: 1},
-  {value: 2}
-]
 
 </script>
 
@@ -26,30 +15,7 @@ const items = [
       </v-switch>
     </template>
   </v-btn>
-  <v-btn class="ml-1" icon="mdi-information" variant="text"></v-btn>
-  <v-btn size="x-large" class="ml-2" ref="button" v-show="false">
-    Watch query every {{ selectedItems.value }}s
-    <template #prepend>
-      <v-icon>mdi-pencil</v-icon>
-    </template>
-  </v-btn>
-  <v-select
-    :menu-props="{
-    activator: button,
-    openOnClick: true,
-  }"
-    v-model="selectedItems"
-    label="User"
-    :items="items"
-    hide-details
-    return-object
-    v-show="false">
-    <template v-slot:item="{ props, item }">
-      <v-list-item v-bind="props" :title="`${item.value} Seconds`"></v-list-item>
-    </template>
-  </v-select>
-
-
+  <v-btn class="ml-1" icon="mdi-information" variant="text"/>
 </template>
 
 <style scoped>
