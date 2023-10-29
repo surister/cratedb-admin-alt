e94607fcca22dc667c30b730645177666f97358c
surister <surister98@gmail.com>
2023-10-29 13:18:35 +0100

    Add view title component

---
+++ src/components/shared/ViewTitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps(['title'])
+</script>
+
+<template>
+  <v-row>
+    <v-col>
+      <h1>{{ title }}</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped>
+
+</style>
