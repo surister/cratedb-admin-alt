671407ed68930976ca7679273d52e2ba6f54ecbf
surister <surister98@gmail.com>
2023-11-05 15:38:02 +0100

    Refactor html positioning

--- src/components/TheSettingsDrawerConsoleSettings.vue
+++ src/components/TheSettingsDrawerConsoleSettings.vue
@@ -8,29 +8,21 @@ const storedPreferences = use_stored_preferences_store()
       <v-label class="mb-2 mt-3 font-weight-medium">Console settings</v-label>
       <v-row>
         <v-col>
-          <v-text-field
-            label="Font size"
-            v-model:model-value="storedPreferences.console.font_size"
-            suffix="px">
-          </v-text-field>
-          <v-text-field
-            label="Min lines"
-            v-model:model-value="storedPreferences.console.min_lines"
-            suffix="lines">
-          </v-text-field>
+          <v-text-field label="Font size"
+                        v-model:model-value="storedPreferences.console.font_size"
+                        suffix="px"/>
+          <v-text-field label="Min lines"
+                        v-model:model-value="storedPreferences.console.min_lines"
+                        suffix="lines"/>
         </v-col>
         <v-col>
-          <v-text-field
-            label="Query limit"
-            v-model:model-value="storedPreferences.console.query_limit"
-            model-value="100"
-            suffix="rows"
-          ></v-text-field>
-          <v-text-field
-            label="Max lines"
-            v-model:model-value="storedPreferences.console.max_lines"
-            suffix="lines">
-          </v-text-field>
+          <v-text-field label="Query limit"
+                        v-model:model-value="storedPreferences.console.query_limit"
+                        model-value="100"
+                        suffix="rows"/>
+          <v-text-field label="Max lines"
+                        v-model:model-value="storedPreferences.console.max_lines"
+                        suffix="lines"/>
         </v-col>
       </v-row>
 </template>
