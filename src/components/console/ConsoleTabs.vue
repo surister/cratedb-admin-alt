db43902bb799003aa0d9f99665dd0c1eedafd183
surister <surister98@gmail.com>
2023-11-06 15:27:42 +0100

    Add that when create a new tab, we auto-select that tab

--- src/components/console/ConsoleTabs.vue
+++ src/components/console/ConsoleTabs.vue
@@ -3,26 +3,29 @@ import {ref} from "vue";
 import {use_console_store} from "@/store/console_store";
 
 const console_store = use_console_store()
-const tab = ref(null)
+
+function add_tab(){
+  console_store.consoles.push({
+    content: '',
+    name: `new_query(${console_store.consoles.length + 1})`
+  })
+  console_store.current_console_index = console_store.consoles.length - 1
+}
 </script>
 
 <template>
-    <v-tabs v-model="tab" density="comfortable" color="primary">
-      <v-btn
-        variant="text"
-        height="100%"
-        class="align-self-center"
-        @click="console_store.consoles.push({content:'', name:`new_query(${console_store.consoles.length + 1})`})">
-        New
-        <v-icon end>
-          mdi-plus
-        </v-icon>
-      </v-btn>
+    <v-tabs v-model="console_store.current_console_index" density="comfortable" color="primary">
+      <v-btn variant="text"
+             height="100%"
+             class="align-self-center"
+             @click="add_tab"
+            text="new"
+            append-icon="mdi-plus"/>
       <v-tab
         class="border-e-sm"
         v-for="(con, i) in console_store.consoles"
         :key="i"
-        :value="i + 1"
+        :value="i"
         @click="console_store.current_console_index = i">
         <template #default>
           <v-row align="center">
