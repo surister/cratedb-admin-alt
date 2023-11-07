a5f9e109f79f3d6dc8b5e2419d99294b00750a76
surister <surister98@gmail.com>
2023-11-07 15:49:04 +0100

    Refactor for linting

--- src/components/console/ConsoleActions.vue
+++ src/components/console/ConsoleActions.vue
@@ -5,6 +5,7 @@ import SubmitBtn from "@/components/console/ConsoleActionsSubmitBtn.vue";
 import CancelQueryBtn from "@/components/console/ConsoleActionsCancelQueryBtn.vue";
 import QueryHistorySwitch from "@/components/console/ConsoleActionsHistorySwitch.vue";
 import FormatQueryBtn from "@/components/console/ConsoleActionsFormatQueryBtn.vue";
+
 import {use_console_store} from "@/store/console_store";
 
 const console_store = use_console_store()
@@ -15,26 +16,27 @@ const console_store = use_console_store()
     <v-container fluid>
       <v-row align="center">
         <v-col cols="2">
-            <v-text-field
-                    density="compact"
-                    v-model="console_store.consoles[console_store.current_console_index].name"
-                    variant="underlined"
-                    label="query"
-                    single-line
-                    hide-details
-                    clearable>
+          <v-text-field density="compact"
+                        v-model="console_store.consoles[console_store.current_console_index].name"
+                        variant="underlined"
+                        label="query"
+                        single-line
+                        hide-details
+                        clearable>
           </v-text-field>
         </v-col>
-        <v-divider class="mx-1" vertical/>
+        <v-divider class="mx-1"
+                   vertical/>
         <v-col>
-            <submit-btn/>
-            <cancel-query-btn/>
-            <query-history-switch/>
-            <query-watcher/>
-            <format-query-btn/>
+          <submit-btn/>
+          <cancel-query-btn/>
+          <query-history-switch/>
+          <query-watcher/>
+          <format-query-btn/>
         </v-col>
 
-        <v-col class="text-right" cols="1">
+        <v-col class="text-right"
+               cols="1">
           <v-btn variant="tonal"
                  icon="mdi-history"
                  @click="console_store.history_drawer = !console_store.history_drawer"/>
--- src/components/console/ConsoleTabs.vue
+++ src/components/console/ConsoleTabs.vue
@@ -1,10 +1,9 @@
 <script setup>
-import {ref} from "vue";
 import {use_console_store} from "@/store/console_store";
 
 const console_store = use_console_store()
 
-function add_tab(){
+function add_tab() {
   console_store.consoles.push({
     content: '',
     name: `new_query(${console_store.consoles.length + 1})`
@@ -14,34 +13,32 @@ function add_tab(){
 </script>
 
 <template>
-    <v-tabs v-model="console_store.current_console_index" density="comfortable" color="primary">
-      <v-btn variant="text"
-             height="100%"
-             class="align-self-center"
-             @click="add_tab"
-            text="new"
-            append-icon="mdi-plus"/>
-      <v-tab
-        class="border-e-sm"
-        v-for="(con, i) in console_store.consoles"
-        :key="i"
-        :value="i"
-        @click="console_store.current_console_index = i">
-        <template #default>
-          <v-row align="center">
-            <v-col>{{ con.name }}</v-col>
-            <v-col class="text-right px-0">
-              <v-btn icon="mdi-close"
-                     :disabled="i === 0"
-                     size="x-small"
-                     variant="text"
-                     :ripple="false"
-                     @click.stop="console_store.consoles.splice(i, 1)"/>
-            </v-col>
-          </v-row>
-        </template>
-      </v-tab>
-    </v-tabs>
+  <v-tabs v-model="console_store.current_console_index" density="comfortable" color="primary">
+    <v-btn height="100%"
+           class="align-self-center rounded-0"
+           @click="add_tab"
+           text="new"
+           append-icon="mdi-plus"/>
+    <v-tab class="border-e-sm"
+           v-for="(con, i) in console_store.consoles"
+           :key="i"
+           :value="i"
+           @click="console_store.current_console_index = i">
+      <template #default>
+        <v-row align="center">
+          <v-col>{{ con.name }}</v-col>
+          <v-col class="text-right px-0">
+            <v-btn icon="mdi-close"
+                   :disabled="i === 0"
+                   size="x-small"
+                   variant="text"
+                   :ripple="false"
+                   @click.stop="console_store.consoles.splice(i, 1)"/>
+          </v-col>
+        </v-row>
+      </template>
+    </v-tab>
+  </v-tabs>
 </template>
 
 <style scoped>
