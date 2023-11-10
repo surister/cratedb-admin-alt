ad7a82475eedffe8b1cd93661bdce3d56d584647
surister <surister98@gmail.com>
2023-11-10 12:45:42 +0100

    Don't allow to submit content with keybind CTRL+ENTER if there is no content

--- src/views/Console.vue
+++ src/views/Console.vue
@@ -27,8 +27,11 @@ const console_store = use_console_store()
   </v-row>
   <v-row no-gutters>
     <v-col>
-      <console-editor @keydown.shift.enter.prevent="console_store.query_from_console()"
-                      v-model:content="console_store.consoles[console_store.current_console_index].content"/>
+      <console-editor v-model:content="console_store.consoles[console_store.current_console_index].content"
+                      @keydown.shift.enter.prevent="()=>{
+        if(console_store.current_console.content !== ''){console_store.query_from_console()}
+      }"
+      />
       <console-response v-if="console_store.response.type !== ''"/>
       <console-table-results class="mt-4"/>
     </v-col>
