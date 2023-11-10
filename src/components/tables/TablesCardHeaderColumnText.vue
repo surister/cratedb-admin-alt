2b302796e0004afbcd31d876e479dcdd3e387a4e
surister <surister98@gmail.com>
2023-11-10 15:09:53 +0100

    Improve readability of long numbers

--- src/components/tables/TablesCardHeaderColumnText.vue
+++ src/components/tables/TablesCardHeaderColumnText.vue
@@ -11,13 +11,24 @@ const props = defineProps({
     required: false
   },
 })
+
+const formatted_value = () => {
+  if (props.value == null) {
+    return 'n/a'
+  }
+  if (Number.isInteger(props.value)) {
+    // We format integers such as '100000' to '100 000' so they can be read easily.
+    return props.value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ")
+  }
+  return props.value
+}
 </script>
 
 <template>
   <v-col :cols="cols">
     <h2>{{ title }}</h2>
     <h3 class="pt-2">
-      {{ value === null ? 'n/a' : value }}
+      {{ formatted_value() }}
     </h3>
   </v-col>
 </template>
