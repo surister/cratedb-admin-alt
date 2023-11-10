2f546baab3e4f0bf8fcd8111c340534ce2e6e6b7
surister <surister98@gmail.com>
2023-11-10 12:48:22 +0100

    Add loading effect when the sample data call takes places

--- src/components/tables/TablesCardTabsSample.vue
+++ src/components/tables/TablesCardTabsSample.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { JsonTreeView } from "json-tree-view-vue3";
+import {JsonTreeView} from "json-tree-view-vue3";
 import {adaptVTableHeader, adaptVTableItems, color_objects, is_object} from "@/store/utils";
 import {use_tables_store} from "@/store/tables";
 
@@ -8,20 +8,22 @@ const tables_info = use_tables_store()
 
 <template>
   <v-data-table v-if="tables_info.sample_data != null"
+                :loading="tables_info.sample_data == null"
                 :headers="adaptVTableHeader(tables_info.sample_data.cols)"
                 :items="adaptVTableItems(tables_info.sample_data.rows, tables_info.sample_data.cols)">
-      <template v-slot:item="{ item }">
-        <tr>
-          <td v-for="(it, index) in item" :key="index">
-            <template v-if="is_object(it) || Array.isArray(it)">
-              <json-tree-view class="my-1" colorScheme="dark" rootKey="Object" maxDepth="0" :data="JSON.stringify(it)"></json-tree-view>
-            </template>
-            <template v-else>
-              <span :style="{color: color_objects(it)}">{{ it }} </span>
-            </template>
-          </td>
-        </tr>
-      </template>
+    <template v-slot:item="{ item }">
+      <tr>
+        <td v-for="(it, index) in item" :key="index">
+          <template v-if="is_object(it) || Array.isArray(it)">
+            <json-tree-view class="my-1" colorScheme="dark" rootKey="Object" maxDepth="0"
+                            :data="JSON.stringify(it)"></json-tree-view>
+          </template>
+          <template v-else>
+            <span :style="{color: color_objects(it)}">{{ it }} </span>
+          </template>
+        </td>
+      </tr>
+    </template>
   </v-data-table>
 </template>
 
