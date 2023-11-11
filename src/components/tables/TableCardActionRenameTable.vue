413e39ca28a8fc451ef7ceb9f84203d9f7d1afd8
surister <surister98@gmail.com>
2023-11-11 12:52:07 +0100

    Refactor table rename with new ButtonWithDialog component

---
+++ src/components/tables/TableCardActionRenameTable.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue";
+import {use_tables_store} from "@/store/tables";
+import ButtonWithDialog from "@/components/shared/buttons/ButtonWithDialog.vue";
+
+const table_store = use_tables_store()
+
+const name = ref()
+</script>
+
+<template>
+  <button-with-dialog tooltip-text="Alter table name"
+                      activator-btn-text="rename"
+                      activator-btn-variant="text"
+                      activator-btn-color=""
+                      dialog-title="New name"
+                      dialog-width="400"
+                      dialog-submit-btn-text="alter"
+                      dialog-submit-btn-variant="text"
+                      :dialog-submit-btn-disabled="name == null"
+                      :submit-callback="() => table_store.rename_table(name)">
+    <template #dialog-content>
+      <v-card-text>
+        <v-text-field variant="outlined"
+                      hint="New table name"
+                      v-model="name"
+                      class="py-2"
+                      clearable/>
+      </v-card-text>
+    </template>
+  </button-with-dialog>
+</template>
+
+<style scoped>
+
+</style>
