ec0856304443cb2d0faeaa78d2464ae9d35d5c01
surister <surister98@gmail.com>
2023-11-13 13:25:06 +0100

    Create VersionNotice.vue component to tell users when a feature is not supported on their CrateDB version

---
+++ src/components/shared/VersionNotice.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  version: {
+    type:String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <v-alert color="yellow" class="my-2">
+    <v-alert-title>Feature not supported!</v-alert-title>
+    Your CrateDB node does not support this feature, it is only available in >={{ version }}
+  </v-alert>
+</template>
+
+<style scoped>
+
+</style>
