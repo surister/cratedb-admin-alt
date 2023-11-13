124175d7e39fc05bb747ed2f053760ac869bc664
surister <surister98@gmail.com>
2023-11-13 20:32:12 +0100

    Includi missing shards and underreplicated shards in heatlh info

--- src/components/TheAppBarHealthPopOver.vue
+++ src/components/TheAppBarHealthPopOver.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import {use_node_info_store} from "@/store/node_info";
+import {computed} from "vue";
 
 const node_info_store = use_node_info_store()
 
@@ -14,6 +15,13 @@ const props = defineProps(
       allocations_by_health: Array[Object]
     }
 )
+
+const allocation_issues = computed(() =>{
+  let t = []
+  for (const issue of node_info_store.allocations.get_issues_by_table(health.tableName)) {
+    t.push(issue)
+  }
+})
 </script>
 
 <template>
@@ -24,11 +32,11 @@ const props = defineProps(
              :text="message"/>
     <template v-for="health in healths"
               :key="health.tableName">
-      <p class="mt-5 pb-4">Found the following issues:</p>
-      <template
-          v-for="issue in node_info_store.allocations.get_issues_by_table(health.tableName)"
-          :key="issue.shard_id">
+      <p class="mt-5 pb-4">Found the following allocation issues issues on <span class="font-weight-bold">{{ health.table_name }}</span>:</p>
+      <p>There are {{ health.missing_shards }} missing shards and {{ health.under_replicated_shards }} under replicated shards.</p>
+      <template v-for="issue in node_info_store.allocations.get_issues_by_table(health.tableName)" :key="issue.shard_id">
         <v-divider/>
+        {{ healths }}
         <div class="pa-4">
           Table `<strong>{{ issue.table_name }}</strong>` (shard {{ issue.shard_id }}):
           <v-label>{{ issue.explanation }}.</v-label>
@@ -41,6 +49,7 @@ const props = defineProps(
             Not explained by the node.
           </template>
         </div>
+
       </template>
     </template>
   </v-card>
