51c9d0c8203314ca08662690e65167746f8c2143
surister <surister98@gmail.com>
2023-11-13 11:34:22 +0100

    Add response handling logic + add disabled, icon, response component type prop

--- src/components/shared/buttons/ButtonWithDialog.vue
+++ src/components/shared/buttons/ButtonWithDialog.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import {ref} from "vue";
+import {use_global_store} from "@/store/global_store";
 
 const props = defineProps({
   tooltipText: {
@@ -12,9 +13,17 @@ const props = defineProps({
     required: false,
     default: 'top'
   },
+  activatorBtnDisabled: {
+    type: Boolean,
+    default: false
+  },
   activatorBtnText: {
     type: String,
-    default: 'Yooo',
+    default: 'activator',
+  },
+  activatorBtnIcon: {
+    type: String,
+    default: null
   },
   activatorBtnColor: {
     type: String,
@@ -58,20 +67,57 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  dialogResponseComponent: {
+    type: String, // Accepts 'banner and snackbar'
+    default: 'alert'
+  },
+  dialogOverrideSuccessComponentMessage: {
+    type: String,
+    default: null
+  },
+  dialogResponseComponentMessage: {
+    type: Object,
+    default: {
+      success: 'Success!',
+      error: 'Something went wrong!'
+    }
+  },
   submitCallback: {
     type: Function,
     required: false,
   }
 })
-const result = ref(null)
+const result = ref({})
 
 function handleClick() {
   // We expect the received function to ultimately return a dictionary.
-  props.submitCallback().then((res) => {
-    result.value = res
-  })
+  props.submitCallback().then(async (res) => {
+      result.value = {}
+    if (res.ok) {
+      result.value['type'] = 'success'
+      result.value['title'] = props.dialogOverrideSuccessComponentMessage != null ? props.dialogOverrideSuccessComponentMessage : props.dialogResponseComponentMessage['success']
+    } else {
+      result.value['type'] = 'error'
+      result.value['title'] = props.dialogResponseComponentMessage['error']
 
-  emit("onSubmit")
+      const data = await res.json()
+      result.value['message'] = data.error.message
+    }
+
+    emit("onSubmit")
+
+    if (props.dialogResponseComponent === 'snackbar') {
+      const store = use_global_store()
+      if (result.value.type === 'success') {
+        store.show_successful_snackbar(result.value.title)
+      } else {
+        store.show_error_snackbar(result.value.message)
+      }
+    }
+    if (props.dialogCloseOnSubmit) {
+      dialog.value = false
+    }
+  })
 }
 
 const emit = defineEmits(['onSubmit'])
@@ -82,44 +128,51 @@ const dialog = ref(false)
   <v-tooltip :text="tooltipText"
              :location="tooltipLocation">
     <template v-slot:activator="{ props }">
-      <v-btn :color="activatorBtnColor"
-             :variant="activatorBtnVariant"
-             v-bind="props">
-        {{ activatorBtnText }}
-        <v-dialog v-model="dialog"
-                  activator="parent"
-                  :width="dialogWidth">
-          <v-card>
-            <slot name="dialog-title">
-              <v-toolbar>
-                <v-toolbar-title :text="dialogTitle"/>
-              </v-toolbar>
-            </slot>
-            <slot name="dialog-content">
-              <v-card-text>
-                {{ dialogText }}
-              </v-card-text>
-            </slot>
-            <v-card-text v-if="result != null">
-              <v-alert closable variant="tonal"
-                       :color="result.type"
-                       :title="result.title"
-                       :text="result.subtitle"
-                       @click:close="result = null"/>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer/>
-              <v-btn @click="dialog = false"
-                     :text="dialogCloseBtnText"/>
-              <v-btn :color="dialogSubmitBtnColor"
-                     :text="dialogSubmitBtnText"
-                     :variant="dialogSubmitBtnVariant"
-                     :disabled="dialogSubmitBtnDisabled"
-                     @click="handleClick"/>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-btn>
+        <v-btn :color="activatorBtnColor"
+               :variant="activatorBtnVariant"
+               :disabled="activatorBtnDisabled"
+               :icon="activatorBtnIcon"
+               v-bind="props">
+            {{ activatorBtnText }}
+            <template v-if="activatorBtnIcon">
+                <v-icon>{{ activatorBtnIcon }}</v-icon>
+            </template>
+            <v-dialog v-model="dialog"
+                      activator="parent"
+                      :width="dialogWidth">
+                <v-card>
+                    <slot name="dialog-title">
+                        <v-toolbar>
+                            <v-toolbar-title :text="dialogTitle"/>
+                        </v-toolbar>
+                    </slot>
+                    <slot name="dialog-content">
+                        <v-card-text>
+                            {{ dialogText }}
+                        </v-card-text>
+                    </slot>
+                    <template v-if="dialogResponseComponent === 'alert'">
+                        <v-card-text v-if="result.type != null">
+                            <v-alert closable variant="tonal"
+                                     :color="result.type"
+                                     :title="result.title"
+                                     :text="result.message"
+                                     @click:close="result = {}"/>
+                        </v-card-text>
+                    </template>
+                    <v-card-actions>
+                        <v-spacer/>
+                        <v-btn @click="dialog = false"
+                               :text="dialogCloseBtnText"/>
+                        <v-btn :color="dialogSubmitBtnColor"
+                               :text="dialogSubmitBtnText"
+                               :variant="dialogSubmitBtnVariant"
+                               :disabled="dialogSubmitBtnDisabled"
+                               @click="handleClick"/>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-btn>
     </template>
   </v-tooltip>
 </template>
