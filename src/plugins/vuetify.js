d276e64f4f26ff59581c82e73d83e8c9d4bae179
surister <surister98@gmail.com>
2023-11-14 17:46:41 +0100

    Update dependencies

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,21 +11,8 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
-import {
-  VDataTable,
-  VDataTableServer,
-  VDataTableVirtual,
-} from "vuetify/labs/VDataTable";
-import {VSkeletonLoader} from "vuetify/labs/components";
-
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  components: {
-    VDataTable,
-    VDataTableServer,
-    VDataTableVirtual,
-    VSkeletonLoader
-  },
   theme: {
     themes: {
       light: {
