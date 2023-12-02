a4f608efe80c05c6b9b6b258e3e25bd376ec6a29
surister <surister98@gmail.com>
2023-12-02 21:10:56 +0100

    Add restore snapshot functionality

--- src/store/repositories.js
+++ src/store/repositories.js
@@ -28,7 +28,7 @@ export const use_repositories_store = defineStore('repositories', () => {
     return response
   }
 
-  async function create_repository(repository_name, repository_type, options){
+  async function create_repository(repository_name, repository_type, options) {
     //CREATE REPOSITORY myrepo2 TYPE fs WITH (location = '/data/backup/repo2')
     const opts = `location = '${options.location}'`
 
@@ -38,14 +38,14 @@ export const use_repositories_store = defineStore('repositories', () => {
       '%options': opts
     })
 
-    if (response.ok){
+    if (response.ok) {
       await update_repositories()
     }
     return response
   }
 
-  async function create_snapshot(options){
-    const { name, wait_for_completion, ignore_unavailable, all_tables, tables } = options
+  async function create_snapshot(options) {
+    const {name, wait_for_completion, ignore_unavailable, all_tables, tables} = options
     const response = await request_crate(queries.CREATE_SNAPSHOT, null, {
       '%repository_name': `"${state.current_open_repository.name}"."${name.replace(' ', '_')}"`,
       '%tables': all_tables ? 'ALL' : tables,
@@ -55,28 +55,34 @@ export const use_repositories_store = defineStore('repositories', () => {
     })
 
     if (response.ok) {
-        await update_repositories()
-        // A  way of updating the state locally of a .current_opened_something.
-        // In tables and users we handle the data manually, but doing so in here would be inaccurate
-        // since we actually need data from the server that we cannot know beforehand,
-        // such as finished time, failures...
-        for (const repository of state.repositories.repositories) {
-          if (repository.name === state.current_open_repository.name){
-            state.current_open_repository = repository
-          }
+      await update_repositories()
+      // A  way of updating the state locally of a .current_opened_something.
+      // In tables and users we handle the data manually, but doing so in here would be inaccurate
+      // since we actually need data from the server that we cannot know beforehand,
+      // such as finished time, failures...
+      for (const repository of state.repositories.repositories) {
+        if (repository.name === state.current_open_repository.name) {
+          state.current_open_repository = repository
         }
+      }
     }
     return response
   }
 
-  async function drop_snapshot(snapshot_name){
+  async function drop_snapshot(snapshot_name) {
     const response = await request_crate(queries.DROP_SNAPSHOT, null, {
       '%snapshot_name': snapshot_name,
       '%repository_name': state.current_open_repository.name
     })
-    if (response.ok){
+    if (response.ok) {
       state.current_open_repository.remove_snapshot(snapshot_name)
     }
+    return response
+  }
+
+  async function restore_snapshot(generated_sql) {
+    const response = await request_crate(generated_sql)
+
     return response
   }
 
@@ -86,7 +92,7 @@ export const use_repositories_store = defineStore('repositories', () => {
     drop_repository,
     create_repository,
     create_snapshot,
-    drop_snapshot
+    drop_snapshot,
+    restore_snapshot
   }
-
 })
