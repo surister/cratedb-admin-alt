4bee178e9b59118b8244d987ec0f1ab042e8f71b
surister <surister98@gmail.com>
2024-01-03 12:13:49 +0100

    Hide columns that we have not yet figured out, will come back at this sometime later on; I have some idea on how to do it, but I'd first need some way of forcing a CrateDB cluster to be unhealthy with data to properly test this.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@ import DurationChart from "@/components/home/DurationChart.vue";
 import {use_tables_store} from "@/store/tables";
 import {computed} from "vue";
 import {CRATE_HEALTH_LEGEND} from "@/store/crate_api/crate_lang";
+import {separate_integer_by} from "@/store/utils";
 
 const tables_store = use_tables_store()
 const health = computed(() => tables_store.schemas.get_current_health_level())
@@ -16,29 +17,49 @@ const health = computed(() => tables_store.schemas.get_current_health_level())
     <v-card>
       <v-card-text>
         <v-row>
-          <v-col>Overall Cluster health<br>
+          <v-col cols="2">
+            Overall Cluster health<br>
+            <v-chip label
+                    class="mt-2"
+                    :text="CRATE_HEALTH_LEGEND[health].short_description"
+                    :color="health.toString().toLocaleLowerCase()"/>
+          </v-col>
+
+          <v-col v-if="false">
+            Replicated Data<br>
+            <v-chip label
+                    class="mt-2"
+                    :text="CRATE_HEALTH_LEGEND[health].short_description"
+                    :color="health.toString().toLocaleLowerCase()"/>
+          </v-col>
+
+          <v-col v-if="false">
+            Available data<br>
+            <v-chip label
+                    class="mt-2"
+                    :text="CRATE_HEALTH_LEGEND[health].short_description"
+                    :color="health.toString().toLocaleLowerCase()"/>
+          </v-col>
+
+          <v-col cols="2">
+            Total records<br>
+            <v-chip label
+                    class="mt-2"
+                    :text="separate_integer_by(tables_store.schemas.get_total_records(), ' ')"/>
+          </v-col>
+
+          <v-col v-if="false">
+            Under-replicated records<br>
             <v-chip label class="mt-2" :text="CRATE_HEALTH_LEGEND[health].short_description"
                     :color="health.toString().toLocaleLowerCase()"></v-chip>
           </v-col>
-          <v-col>Replicated Data<br>
+
+          <v-col v-if="false">
+            Unavailable records<br>
             <v-chip label class="mt-2" :text="CRATE_HEALTH_LEGEND[health].short_description"
                     :color="health.toString().toLocaleLowerCase()"></v-chip>
           </v-col>
-          <v-col>Available data<br>
-            <v-chip label class="mt-2" :text="CRATE_HEALTH_LEGEND[health].short_description"
-                    :color="health.toString().toLocaleLowerCase()"></v-chip>
-          </v-col>
-          <v-col>Total records<br>
-            <v-chip label class="mt-2" :text="tables_store.schemas.get_total_records()"></v-chip>
-          </v-col>
-          <v-col>Under-replicated records<br>
-            <v-chip label class="mt-2" :text="CRATE_HEALTH_LEGEND[health].short_description"
-                    :color="health.toString().toLocaleLowerCase()"></v-chip>
-          </v-col>
-          <v-col>Unavailable records<br>
-            <v-chip label class="mt-2" :text="CRATE_HEALTH_LEGEND[health].short_description"
-                    :color="health.toString().toLocaleLowerCase()"></v-chip>
-          </v-col>
+
         </v-row>
       </v-card-text>
     </v-card>
