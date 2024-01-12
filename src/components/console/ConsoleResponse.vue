5d89e508eaedf0eca45a99d77a6797db575f2e60
surister <surister98@gmail.com>
2024-01-12 21:20:24 +0100

    Improve the styling of the response message, add proper borders & roundiness depending on the response type

--- src/components/console/ConsoleResponse.vue
+++ src/components/console/ConsoleResponse.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TracebackDialog from "@/components/console/ConsoleResponseTracebackDialog.vue";
 import {use_stored_preferences_store} from "@/store/storedPreferences";
+import {computed} from "vue";
 
 const stored_preferences_store = use_stored_preferences_store()
 
@@ -12,26 +13,34 @@ const props = defineProps(
       }
     }
 )
+const is_success = computed(()=> props.data.type === 'success')
 </script>
 
 <template>
-  <v-card v-if="data.type" class="rounded-0 border-s-sm border-e-sm">
-    <template #text>
+  <v-card v-if="data.type"
+          :class="[is_success ? '' : 'border-b-sm','border-s-sm', 'border-e-sm']"
+          :rounded="is_success ? 0: ''"
+          elevation="0">
 
-      <span :class="[ data.type === 'success' ? 'text-green-accent-3' : 'text-red-accent-4', 'text-h5' ]">
-        {{ data.title }}
-      </span> -
+    <v-card-text>
+      [{{ data.timestamp }}]:
+      <v-chip label size="small" class="mb-1" color="info"
+              v-if="stored_preferences_store.experimental_query_limit">Limited at
+        {{ stored_preferences_store.console.query_limit }} row(s)
+      </v-chip>
+      <span :class="[ data.type === 'success' ? 'text-green-accent-3' : 'text-red-accent-4', 'mx-1', 'pb-2'] ">
+                  <v-icon :icon="data.type === 'success' ? 'mdi-check' : 'mdi-alert-decagram'"></v-icon>
+            </span>
+      <span class="text-h7">{{ data.subtitle }}</span>
+    </v-card-text>
 
-      <v-chip label size="small" class="mx-2 mb-1" color="info" v-if="stored_preferences_store.experimental_query_limit">Limited at {{ stored_preferences_store.console.query_limit }} row(s)</v-chip>
-      <span class="text-h6">{{ data.subtitle }}</span>
-
-    </template>
 
     <v-card-actions v-if="data.error_trace">
 
-      <traceback-dialog  :text="data.error_trace"/>
+      <traceback-dialog :text="data.error_trace"/>
 
     </v-card-actions>
+
   </v-card>
 
 </template>
