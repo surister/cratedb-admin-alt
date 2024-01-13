44dfcd3439ec112bda5446a465a19d068130afb7
surister <surister98@gmail.com>
2024-01-13 15:41:58 +0100

    Fix padding of loads

--- src/components/TheAppBarInfoLoad.vue
+++ src/components/TheAppBarInfoLoad.vue
@@ -1,15 +1,12 @@
 <script setup>
 import {use_node_info_store} from "@/store/node_info";
-import {computed} from "vue";
-
 const node_info_store = use_node_info_store()
 
 const to_fixed = (load) => {
-  console.log(load)
-  if (load instanceof String) {
-    return load.toFixed(2)
+  if (typeof load === 'string' || load instanceof String) {
+    return load
   }
-  return load
+  return load.toFixed(2)
 }
 </script>
 
@@ -18,16 +15,17 @@ const to_fixed = (load) => {
   <v-label class="pr-2 text-subtitle-2">System Load</v-label>
     <v-chip label size="small">
       <p class="font-weight-bold" style="font-size: 17px">
-        <template
-          v-if="node_info_store.nodes.has_nodes() && node_info_store.nodes.nodes[0].load.load1 != null">
 
+        <template v-if="node_info_store.nodes.has_nodes() && node_info_store.nodes.nodes[0].load.load1 != null">
           {{ to_fixed(node_info_store.nodes.get_master_node().load.load1) }} /
           {{ to_fixed(node_info_store.nodes.get_master_node().load.load5) }} /
           {{ to_fixed(node_info_store.nodes.get_master_node().load.load15) }}
         </template>
+
         <template v-else>
           load/load/load
         </template>
+
       </p>
     </v-chip>
 </span>
