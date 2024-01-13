14aaa3985a61b3e813b8bab0cf773a349b2fa03d
surister <surister98@gmail.com>
2024-01-13 13:33:47 +0100

    Refactor component into btn-with-dialog

--- src/components/console/ConsoleActionSaveQuery.vue
+++ src/components/console/ConsoleActionSaveQuery.vue
@@ -2,51 +2,37 @@
 import {use_console_store} from "@/store/console_store";
 import {use_stored_preferences_store} from "@/store/storedPreferences";
 import {ref} from "vue";
+import ButtonWithDialog from "@/components/shared/buttons/ButtonWithDialog.vue";
 
 const console_store = use_console_store()
 const stored_preferences_store = use_stored_preferences_store()
-
-const dialog = ref(false)
 const name = ref()
-
 </script>
 
 <template>
-  <v-tooltip text="Save query" location="top">
-    <template v-slot:activator="{ props }">
-      <v-btn class="ml-2"
-             variant="tonal"
-             icon="mdi-content-save"
-             v-bind="props"
-             size="small"
-             :disabled="!(console_store.current_console.content !== '')"
-             @click="dialog = true">
-      </v-btn>
-    </template>
-  </v-tooltip>
-  <v-dialog v-model="dialog"
-            max-width="400">
-    <v-card>
+  <button-with-dialog tooltip-text="Save query"
+                      activator-btn-color=""
+                      activator-btn-icon="mdi-content-save"
+                      activator-btn-text=""
+                      dialog-width="350"
+                      dialog-override-success-component-message="Query added"
+                      dialog-submit-btn-color="primary"
+                      dialog-submit-btn-text="Save"
+                      :dialog-submit-btn-disabled="!name"
+                      :activator-btn-disabled="!console_store.current_console.content"
+                      :submit-callback="() => stored_preferences_store.save_query(name, console_store.current_console.content)">
+    <template #dialog-content>
       <v-card-text>
         <v-label class="pb-4" text="Name"/>
         <v-text-field :autofocus="true"
+                      variant="outlined"
+                      hide-details
                       v-model="name"/>
-        <v-card-actions class="text-right">
-          <v-spacer/>
-          <v-btn color="primary"
-                 @click="stored_preferences_store.save_query(name, console_store.current_console.content); dialog = false"
-                 :disabled="name == null"
-                 text="save"/>
-          <v-btn text="close"
-                 @click="dialog = false"/>
-        </v-card-actions>
       </v-card-text>
+    </template>
 
-    </v-card>
-
-  </v-dialog>
+  </button-with-dialog>
 </template>
-
 <style scoped>
 
 </style>
