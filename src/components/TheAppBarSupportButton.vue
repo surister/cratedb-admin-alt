7f2e34221a51c9918bf466d07e2f43d4e23abbdf
surister <surister98@gmail.com>
2024-01-13 15:40:57 +0100

    Add links to support button

--- src/components/TheAppBarSupportButton.vue
+++ src/components/TheAppBarSupportButton.vue
@@ -1,10 +1,30 @@
 <script setup>
 const items = [
-  {title: 'Github', icon: 'mdi-github', link: ''},
-  {title: 'Stack Overflow', icon: 'mdi-stack-overflow', link: ''},
-  {title: 'Documentation', icon: 'mdi-file-document-outline', link: ''},
-  {title: 'Forum', icon: 'mdi-forum', link: ''},
-  {title: 'Latest release', icon: 'mdi-alert-decagram', link: ''},
+  {
+    title: 'Github',
+    icon: 'mdi-github',
+    link: 'https://github.com/crate'
+  },
+  {
+    title: 'Stack Overflow',
+    icon: 'mdi-stack-overflow',
+    link: 'https://stackoverflow.com/questions/tagged/cratedb'
+  },
+  {
+    title: 'Documentation',
+    icon: 'mdi-file-document-outline',
+    link: 'https://cratedb.com/docs/crate/reference/en/latest/'
+  },
+  {
+    title: 'Forum',
+    icon: 'mdi-forum',
+    link: 'https://community.cratedb.com/'
+  },
+  {
+    title: 'Latest release',
+    icon: 'mdi-alert-decagram',
+    link: 'https://cratedb.com/docs/crate/reference/en/latest/appendices/release-notes/'
+  },
 ]
 </script>
 
@@ -14,18 +34,26 @@ const items = [
           open-delay="60"
           open-on-hover
           class="border-sm">
+
     <template v-slot:activator="{ props }">
-      <v-btn class="ml-2" v-bind="props" text="Support" variant="tonal" size="small"/>
+      <v-btn class="ml-2"
+             v-bind="props"
+             text="Support"
+             variant="tonal"
+             size="small"/>
     </template>
 
     <v-list>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        :prepend-icon="item.icon"
-        :link="true"
-        :title="item.title">
+
+      <v-list-item v-for="item in items"
+                   :key="item.title"
+                   :prepend-icon="item.icon"
+                   :link="true"
+                   :title="item.title"
+                   target="_blank"
+                   :href="item.link">
       </v-list-item>
+
     </v-list>
   </v-menu>
 
