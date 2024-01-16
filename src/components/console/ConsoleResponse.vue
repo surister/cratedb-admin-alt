17d02e5932cac7dcdad901d44d914b5ca2a82bbf
surister <surister98@gmail.com>
2024-01-16 20:15:30 +0100

    Show only bottom border in the console card if there is rows to show

--- src/components/console/ConsoleResponse.vue
+++ src/components/console/ConsoleResponse.vue
@@ -6,21 +6,21 @@ import {computed} from "vue";
 const stored_preferences_store = use_stored_preferences_store()
 
 const props = defineProps(
-    {
-      data: {
-        type: Object,
-        default: null
-      }
+  {
+    data: {
+      type: Object,
+      default: null
     }
+  }
 )
-const is_success = computed(()=> props.data.type === 'success')
+const is_success = computed(() => props.data.type === 'success')
 </script>
 
 <template>
   <v-card v-if="data.type"
           rounded="0"
-          elevation="0">
-
+          elevation="0"
+          :class="data.data.headers.length > 0 ? 'border-b-sm': ''">
     <v-card-text class="ma-0">
       <v-icon size="small">mdi-console-line</v-icon>
       [{{ data.timestamp }}]:
@@ -30,8 +30,8 @@ const is_success = computed(()=> props.data.type === 'success')
       </v-chip>
       <span
         :class="[ data.type === 'success' ? 'text-green-accent-3' : 'text-red-accent-4', 'mx-1', 'pb-2'] ">
-                  <v-icon :icon="is_success ? 'mdi-check' : 'mdi-alert-decagram'"></v-icon>
-            </span>
+                  <v-icon :icon="is_success ? 'mdi-check' : 'mdi-alert-decagram'"/>
+      </span>
       <span class="text-h7">{{ data.subtitle }}</span>
     </v-card-text>
 
